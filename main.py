169766e20a866c71e626366b6f31ead029a65175
oudotje <jean.oudot.06@hotmail.com>
2024-12-22 22:01:07 +0700

    Add a pretty print

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def pretty_print(count_chars):
+    for k in count_chars: 
+        print(f"The {k} character was found {count_chars[k]} times.")
+
+def count_char(input):
+    input_lower = input.lower()
+    results = {}
+    
+    for c in input_lower:
+        if c in results:
+            results[c] += 1
+        else:
+            results[c] = 1
+    return results
+
 def count_words(input):
     return len(input.split())
 
@@ -7,6 +22,8 @@ def main():
         print(file_contents)
         words = count_words(file_contents)
         print(words)
+        count_chars = count_char(file_contents)
+        pretty_print(count_chars)
 
 main()        
 
